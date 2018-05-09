5696fc72c130091ce7c0583cf63b0ce822de6a92
Joanne Lee <vernapii@gmail.com>
2018-05-09 12:21:33 -0700

    minor edits made in readme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-#   configure do
-#     set :public_folder, 'public'
-#     set :views, 'app/views'
-#     enable :sessions unless test?
-#     set :session_secret, "secret"
-#   end
-#
-#   get '/' do
-#     erb :index
-#   end
-#
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "confidant_secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
 #   helpers do
 #   end
 end
