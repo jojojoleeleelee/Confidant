d927536409703f171627721bbdc27216d4ee5589
Joanne Lee <vernapii@gmail.com>
2018-05-09 17:32:55 -0700

    users controller complete

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Confidant
 
-Confidant is a web application, serving those in need of a confidant. To quote our good 'ol buddy William Wordsworth, feel free to use this platform to 'fill your paper with the breathings of your heart.'
+Confidant is a web application, serving those in need of a confidant.
+
+To quote our good 'ol buddy William Wordsworth, feel free to use this platform to 'fill your paper with the breathings of your heart.'
 
 This is a platform that will guide the user through their current emotions and enable them to elaborate their concerns in further detail. All of this is then posted to the user's personal dashboard.
 
 Last but not least, Confidant will serve as a trustworthy confidant by giving the user customized feedback with words of true wisdom - quoting verses from the Bible.
 
-- Build by **Joanne Lee**
+
+- Built by **Joanne Lee**
--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -1,3 +1,73 @@
 class EmotionsController < ApplicationController
-  
+  get "/emotions" do
+    if logged_in?
+      @emotions = Emotion.all
+      erb :"/emotions/emotions"
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/emotions/new' do
+    if logged_in?
+      erb :"/emotions/create_emotion"
+    else
+      redirect "/login"
+    end
+  end
+
+  post '/emotions' do
+    if !params[:content].empty?
+      @emotion = Emotion.create(params)
+      @emotion.user = current_user
+      @emotion.user = User.find_by_id(session[:user_id])
+      @emotion.save
+      redirect "/emotions/#{@emotion.id}"
+    else
+      redirect '/emotions/new'
+    end
+  end
+
+
+  get "/emotions/:id" do
+    if logged_in?
+      @emotion = Emotion.find_by_id(params[:id])
+      erb :"/emotions/show_emotions"
+    else
+      redirect "/login"
+    end
+  end
+
+
+  get "/emotions/:id/edit" do
+    if logged_in?
+      @emotion = Emotion.find_by(params[:id])
+      erb :"/emotions/edit_emotions"
+    else
+      redirect "/login"
+    end
+  end
+
+  patch '/emotions/:id' do
+    @user = current_user
+    @emotion = Emotion.find_by(params[:id])
+    if logged_in? && users_emotion? && !params[:content].empty?
+      @emotion.update(content: params[:content])
+      redirect "/emotions/#{@emotion.id}"
+    else
+      redirect "/emotions/#{@emotion.id}/edit"
+    end
+  end
+
+  delete '/emotions/:id/delete' do
+    @emotion = Emotion.find_by(params[:id])
+    @emotion.delete
+    redirect '/emotions'
+  end
+
+  get '/emotions/:id' do
+    @emotion = Emotion.find_by_id(params[:id])
+    @user = User.find_by_id(session[:user_id])
+    erb :"/emotions/show_emotions"
+  end
 end
